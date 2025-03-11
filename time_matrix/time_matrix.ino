504b9bd07ba15377f235638e478e1155edf5f7a9
konodop <150059168+konodop@users.noreply.github.com>
2025-03-11 23:17:30 +0300

    arduino code for matrix

---
+++ time_matrix/time_matrix.ino
@@ -0,0 +1,191 @@
+#include "Adafruit_GFX.h" // библиотеки для работы с адресной лентой
+#include "Adafruit_NeoMatrix.h"
+#include "Adafruit_NeoPixel.h"
+#include <Wire.h>
+#include <iarduino_RTC.h>
+iarduino_RTC watch(RTC_DS1302,6,7,8);  // для модуля DS1302 - RST, CLK, DAT
+#include "DHT.h" // библиотека для датчика DHT11
+
+#define DHT_PIN 2 // порт подключения датчика DHT11
+#define LED_PIN 9  // порт подключения адресной ленты
+#define WIDTH 30  // ширина матрицы в пикселях
+#define ROWS 8      // количество строк в матрице
+boolean humidity = true;          // выводить влажность воздуха - true/false
+unsigned int watchr = 50;             // скорость бегущей строки
+unsigned int watchrs = 80;             // скорость бегущей строки слов
+unsigned int pausatb = 5000;   // время табло для волейбольных счётов
+unsigned int pausa1 = 5000;   // на сколько мсек остановится время на табло
+unsigned int pausa2 = 1000;     // на сколько мсек остановится температура и влажность
+unsigned int brightness = 200;  // яркость матрицы с адресной лентой
+
+// настройки матрицы (если текст выводится некорректно, следует их изменить)
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(WIDTH, ROWS, 1, 1, LED_PIN,
+                            NEO_MATRIX_RIGHT     + NEO_MATRIX_BOTTOM +
+                            NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
+                            NEO_GRB            + NEO_KHZ800);
+
+DHT dht(DHT_PIN, DHT11); // создаем объект для датчика температуры
+byte w, t, h;
+int lsignal=0;
+int llastsignal=0;
+int rsignal=0;
+int rlastsignal=0;
+byte mode = 0;
+int x = WIDTH;
+int left = 0;
+int right = 0;
+
+void setup() {
+  dht.begin();              // запускаем датчик давления
+  Serial.begin(9600); // запускаем монитор порта
+
+  matrix.begin();
+  matrix.fillScreen(0);
+  // установка цветовой палитры адресной ленты (RGB)
+  matrix.setTextColor(matrix.Color(255, 0, 255));
+  matrix.setTextWrap(false);
+  matrix.setBrightness(brightness);
+
+  watch.begin(&Wire);
+  // после установки времени и загрузки скетча в плату Ардуино
+  // закомментируйте следующую строчку и снова загрузите скетч в плату
+//  watch.settime(__TIMESTAMP__); // сек,  мин,  час, число, месяц, год, день недели
+}
+void loop() {
+  Serial.println(watch.gettime("H:i"));
+  variant(); // первый вариант с бегущей строкой
+}
+void scores() {
+  matrix.setBrightness(brightness);
+  matrix.fillScreen(0);
+  w = 1;
+  byte e = 0;
+  while (w == 1) {
+    // левый
+    lsignal=digitalRead(11);
+     if (lsignal!=llastsignal)
+     {
+      if (lsignal==HIGH)
+      {
+        e = 1;
+        left=left+1;
+      }
+      llastsignal=lsignal;
+      if (left==16)
+      {
+        left=0;
+        right=0;
+      }
+     }
+    // правый
+    rsignal=digitalRead(13);
+     if (rsignal!=rlastsignal)
+     {
+      if (rsignal==HIGH)
+      {
+        e = 1;
+        right=right+1;
+      }
+      rlastsignal=rsignal;
+      if (right==16)
+      {
+        right=0;
+        left=0;
+      }
+     }
+    // отображение на матрицу
+    matrix.fillScreen(0);
+    matrix.setCursor(x, 0);
+    if (left < 10) {
+      matrix.print(" ");
+    }
+    matrix.print(left);
+    matrix.print(":");
+    matrix.print(right);
+    matrix.show();
+    delay(watchr);
+    if (x > -1){
+      x--;
+    } else{
+      if (digitalRead(12)) { x = WIDTH; w = 0;}
+    }
+  }
+}
+void variant() {
+  matrix.setBrightness(brightness);
+  matrix.fillScreen(0);
+  w = 1;
+
+  // == выводим время (часы и минуты) на светодиодное табло == //
+  while (w == 1) {
+    matrix.fillScreen(0);
+    matrix.setCursor(x, 0);
+    matrix.print(watch.gettime("H:i"));
+    matrix.show();
+    delay(watchr);
+    int buttonState = digitalRead(12);
+    if (buttonState) {
+      mode = 1;
+      scores();
+    }
+    x--;
+    // когда текст оказывается посередине табло - делаем паузу
+    if (x == -1) { delay(pausa1); }
+    if (x < - WIDTH) { x = WIDTH; w = 2; }
+  }
+
+  // == выводим температуру воздуха на светодиодное табло == //
+  while (w == 2) {
+    t = dht.readTemperature();
+    matrix.fillScreen(0);
+    matrix.setCursor(x, 0);
+    matrix.print(t);
+    matrix.print("'C");
+    matrix.show();
+    delay(watchr);
+    int buttonState = digitalRead(12);
+    if (buttonState) {
+      mode = 1;
+      scores();
+    }
+    x--;
+    // когда текст оказывается посередине табло - делаем паузу
+    if (x == 3) { delay(pausa2); }
+    if (x < - WIDTH && humidity == true) { x = WIDTH; w = 3; }
+    if (x < - WIDTH && humidity == false) { x = WIDTH; w = 0; }
+  }
+
+  // == выводим влажность воздуха на светодиодное табло == //
+  while (w == 3) {
+    h = dht.readHumidity();
+    matrix.fillScreen(0);
+    matrix.setCursor(x, 0);
+    matrix.print(h);
+    matrix.print("%");
+    matrix.show();
+    delay(watchr);
+    int buttonState = digitalRead(12);
+    if (buttonState) {
+      mode = 1;
+      scores();
+    }
+    x--;
+    // когда текст оказывается посередине табло - делаем паузу
+    if (x == 6) { delay(pausa2); }
+    if (x < - WIDTH) { x = WIDTH; w = 4;}
+  }
+  while (w == 4) {
+    matrix.fillScreen(0);
+    matrix.setCursor(x, 0);
+    matrix.print("Licey-internat N1 Almetyevsk");
+    matrix.show();
+    delay(watchrs);
+    int buttonState = digitalRead(12);
+    if (buttonState) {
+      mode = 1;
+      scores();
+    }
+    x--;
+    if (x < -200) { x = WIDTH; w = 5;}
+  }
+}
